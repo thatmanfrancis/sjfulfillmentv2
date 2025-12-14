7a84b201b2eabc1ab4378eaaee2db7ce2e889802
codefromtheslum <kossyuzoigwe@gmail.com>
2025-12-14 11:49:50 +0100

    fixes coming up

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true
-  }
+    ignoreBuildErrors: true,
+  },
   // experimental: {
   //   proxyTimeout: 30000,
   // },
